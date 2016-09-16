e36ddfc0545e4afb66e89a0f58ef4037c862d9b5
Pavel Titov <ptitov@gmail.com>
2016-09-16 10:27:14 +0300

    Revert "Try to fix prod issue with table sorter"
    This reverts commit b051d82bc79e56df9c6f26aeeb7553d47793faca.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # True may be needed for tablesorter to work
-  # XXX Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
