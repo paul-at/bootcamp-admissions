45dae595e0cee8eb1ce9c6593e4f79526cb49c6e
Pavel Titov <ptitov@gmail.com>
2016-09-16 10:25:24 +0300

    Try to fix prod issue with table sorter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,3 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery-tablesorter
-//= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # True may be needed for tablesorter to work
-  # XXX Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.debug = true
+  
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
