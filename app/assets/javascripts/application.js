2e2260eb94c0f7d7e6669ee77450436b5a9ecddc
Pavel Titov <ptitov@gmail.com>
2016-09-16 10:27:05 +0300

    Revert "Try to fix prod issue with table sorter"
    This reverts commit 45dae595e0cee8eb1ce9c6593e4f79526cb49c6e.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery-tablesorter
+//= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # True may be needed for tablesorter to work
-  config.assets.debug = true
-  
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # XXX Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
