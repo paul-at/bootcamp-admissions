b153bf5ec22fdaa727064b5349048bb20ea268a8
Pavel Titov <ptitov@gmail.com>
2016-10-03 19:43:13 +0300

    Correct "Won't come" typo

--- app/helpers/app_form_helper.rb
+++ app/helpers/app_form_helper.rb
@@ -3,4 +3,10 @@ module AppFormHelper
     link_to "<big>#{app_form.average_score}</big> <small>/ #{app_form.klass.max_score}</small>".html_safe,
       app_form_scores_path(app_form_id: app_form.id)
   end
+
+  def event_name(event)
+    t event, scope: [:activerecord, :attributes, :app_form, :event_names], raise: true
+  rescue I18n::MissingTranslationData => e
+    event.to_s.humanize
+  end
 end
