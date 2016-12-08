f17b4e8c165a6028ecdd055899c82c24dc8396aa
Pavel Titov <ptitov@gmail.com>
2016-12-08 04:39:02 +0300

    application.config doesn't seem to be applied to ApplicationMailer from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 Rails.cache.clear
 
 # Initialize ActionMailer after applicaion because Settings are not available until this point
-Rails.application.config.action_mailer.delivery_method = :smtp
-Rails.application.config.action_mailer.smtp_settings = Setting.smtp
-Rails.application.config.action_mailer.default_options = Setting.mail_options
-Rails.application.config.action_mailer.raise_delivery_errors = true
+ApplicationMailer.delivery_method = :smtp
+ApplicationMailer.smtp_settings = Setting.smtp
+ApplicationMailer.default_options = Setting.mail_options
+ApplicationMailer.raise_delivery_errors = true
