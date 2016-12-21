e0d1c01d6a998f7db1c3fe512f89d112cc0ead21
Pavel Titov <ptitov@gmail.com>
2016-12-21 05:31:18 +0300

    Do not count votes towards deleted applications

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,6 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   def self.casted_in(klass)
-    where(app_form: klass.app_forms.where(aasm_state: 'applied')).group(:user_id).count
+    where(app_form: klass.app_forms.visible.where(aasm_state: 'applied')).group(:user_id).count
   end
 end
